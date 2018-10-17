3eb9fd38cac169446b6d89242906a6ce34f2337b
arsh98000 <arsh98000@gmail.com>
2018-10-17 18:48:48 -0500

    Modified the routes file and added navigation throughout the projects.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  get 'home/index'
-  get 'about/index'
-  get 'movies/index'
+  get 'home' => 'home#index'
+  root to: 'home#index'
+  get 'about' => 'about#index'
+  get 'movies' => 'movies#index'
   get 'movies/show'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
