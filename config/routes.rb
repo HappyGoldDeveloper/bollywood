fafb5083e7210a48a0b7ce923c5524818292c7ea
arsh98000 <arsh98000@gmail.com>
2018-10-17 23:12:27 -0500

    modified the routes file and updated show method for Movie.

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,8 @@ Rails.application.routes.draw do
   get 'home' => 'home#index'
   root to: 'home#index'
   get 'about' => 'about#index'
-  get 'movies' => 'movies#index'
-  get 'movies/:id' , to: 'movies#show' , id: /\d+/
+  resources :movies, only:[:index, :show]
+  # get 'movies' => 'movies#index'
+  # get 'movies/:id' , to: 'movies#show' , id: /\d+/
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
