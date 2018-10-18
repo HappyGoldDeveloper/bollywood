1429dcdfcd7d99a86b29a1929f730c1bff13d617
arsh98000 <arsh98000@gmail.com>
2018-10-17 19:14:36 -0500

    Modified the actor controller and the view index for actor.

--- app/controllers/actors_controller.rb
+++ app/controllers/actors_controller.rb
@@ -1,5 +1,7 @@
 class ActorsController < ApplicationController
   def index
+
+    @actors = Actor.all
   end
 
   def show
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'actors/index'
+  get 'actors'=> 'actors#index'
   get 'actors/show'
   get 'home' => 'home#index'
   root to: 'home#index'
