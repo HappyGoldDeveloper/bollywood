dd7b92fccd4b0f80abf9dc95dfb3a49190db9ec8
arsh98000 <arsh98000@gmail.com>
2018-10-17 21:45:50 -0500

    Modified the show method in the movie controller and added involved a primary key.

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,11 @@ Rails.application.routes.draw do
   get 'genres' => 'genres#index'
   get 'genres/show'
   get 'actors'=> 'actors#index'
-  get 'actors/show'
+  get 'actors/:id' => 'actors#show'
   get 'home' => 'home#index'
   root to: 'home#index'
   get 'about' => 'about#index'
   get 'movies' => 'movies#index'
-  get 'movies/show'
+  get 'movies/:id' , to: 'movies#show' , id: /\d+/
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
