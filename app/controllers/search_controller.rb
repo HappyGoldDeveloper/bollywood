a14e3be594589a95c9da2d51ca7929e05789b392
arsh98000 <arsh98000@gmail.com>
2018-10-19 13:55:39 -0500

    Added the Search controller to look for movies with movie title.

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,2 +1,10 @@
 class SearchController < ApplicationController
+
+    def index
+        
+    end
+
+    def results
+        @movies = Movie.where('title LIKE ?', "%#{params[:q]}%")
+    end
 end
