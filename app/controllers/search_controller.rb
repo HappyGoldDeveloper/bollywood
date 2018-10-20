4fe7d9ce9909d971f0f7102e8b5e0a21f05f7440
arsh98000 <arsh98000@gmail.com>
2018-10-19 20:11:37 -0500

    Added the search with respect to the genre.

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -5,6 +5,11 @@ class SearchController < ApplicationController
     end
 
     def results
-        @movies = Movie.where('title LIKE ?', "%#{params[:q]}%")
+        if params[:query_genre][:genre_id].empty?
+            @movies = Movie.where('title LIKE ?', "%#{params[:q]}%")
+        else
+            genre = Genre.find(params[:query_genre][:genre_id])
+            @movies = genre.movies.where('title LIKE ?', "%#{params[:q]}%")
+        end
     end
 end
